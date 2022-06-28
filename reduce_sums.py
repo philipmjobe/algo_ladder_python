ec12cc08c6259995be33b3c1430f24109b67c0ab
Philip <philipmjobe@gmail.com>
2022-06-28 16:50:29 -0400

    adds answer for reduce_sums.py

--- reduce_sums.py
+++ reduce_sums.py
@@ -6,3 +6,14 @@
 # Output: 10
 
 # Explanation: (1 + 2 + 3 + 4) = 10
+
+def calculate_sum(arr):
+    sum = 0
+    for i in arr:
+        sum += i
+    return sum
+
+
+arr = [1, 2, 3, 4]
+res = calculate_sum(arr)
+print(res)
