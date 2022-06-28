303c49fb8521ad09b0d6ed5e4a3b6a3b7195118e
Philip <philipmjobe@gmail.com>
2022-06-28 16:53:23 -0400

    adds less_than_100.py and question

---
+++ less_than_100.py
@@ -0,0 +1,4 @@
+# Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
+
+# Input: [99, 101, 88, 4, 2000, 50]
+# Output: [99, 88, 4, 50]
