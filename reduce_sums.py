3a57fb6574e7d399d1a3dc7653ae1dd8cea5c6b3
Philip <philipmjobe@gmail.com>
2022-06-28 16:27:41 -0400

    adds reduce_sums.py and adds question

---
+++ reduce_sums.py
@@ -0,0 +1,8 @@
+# Write a function that returns the sum of all numbers in a given array.
+
+# NOTE: Do not use any built-in language functions that do this automatically for you.
+
+# Input: [1, 2, 3, 4]
+# Output: 10
+
+# Explanation: (1 + 2 + 3 + 4) = 10
