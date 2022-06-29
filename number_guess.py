c515c24da6afe2d3f377ed046464e27b9e6b2d2f
Philip <philipmjobe@gmail.com>
2022-06-29 14:31:04 -0400

    adds upper and lower bounds and imports random and math

--- number_guess.py
+++ number_guess.py
@@ -3,3 +3,10 @@
 # Build a Number guessing game, in which the user selects a range.
 # Let’s say User selected a range, i.e., from A to B, where A and B belong to Integer.
 # Some random integer will be selected by the system and the user has to guess that integer in the minimum number of guesses
+
+import random
+import math
+
+lower = int(input("Enter Lower Bound:- "))
+
+upper = int(input("Enter Upper Bound:- "))
