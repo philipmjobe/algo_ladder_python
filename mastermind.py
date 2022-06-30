22fea6e25a77d0ab4a77e18b4209b67989e6f09e
Philip <philipmjobe@gmail.com>
2022-06-30 15:19:01 -0400

    adds mastermind.py and implementation instructions

---
+++ mastermind.py
@@ -0,0 +1,14 @@
+# Rules of the game
+# Two players play the game against each other
+# let’s assume Player 1 and Player 2.
+
+# Player 1 plays first by setting a multi-digit number.
+# Player 2 now tries his first attempt at guessing the number.
+# If Player 2 succeeds in his first attempt(despite odds which are highly unlikely) he wins the game and is crowned Mastermind! If not, then Player 1 hints by revealing which digits or numbers Player 2 got correct.
+# The game continues till Player 2 eventually is able to guess the number entirely.
+# Now, Player 2 gets to set the number and Player 1 plays the part of guessing the number.
+# If Player 1 is able to guess the number within a lesser number of tries than Player 2 took, then Player 1 wins the game and is crowned Mastermind.
+# If not, then Player 2 wins the game.
+# The real game, however, has proved aesthetics since the numbers are represented by color-coded buttons.
+
+# We shall not be using any of the Pygame Libraries, to aid us with additional graphics, and therefore shall be dealing only with the framework and concept. Furthermore, we are going to be playing against the Computer i.e, the Computer would generate the number to be guessed.
