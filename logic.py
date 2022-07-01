3c71564befc39001f7e3773afb09f64a6a105bb9
Philip <philipmjobe@gmail.com>
2022-07-01 14:51:31 -0400

    fixes spelling of transpose and adds movement functions

--- logic.py
+++ logic.py
@@ -102,10 +102,49 @@ def reverse(mat):
     return new_mat
 
 
-def tanspose(mat):
+def transpose(mat):
     new_mat = []
     for i in range(4):
         new_mat.append([])
         for j in range(4):
             new_mat[i].append(mat[j][i])
     return new_mat
+
+
+def move_left(grid):
+
+    new_grid, changed1 = compress(grid)
+
+    new_grid, changed2 = merge(new_grid)
+
+    changed = changed1 or changed2
+
+    new_grid, temp = compress(new_grid)
+
+    return new_grid, changed
+
+
+def move_right(grid):
+
+    new_grid = reverse(grid)
+
+    new_grid, changed = move_left(new_grid)
+
+    new_grid = reverse(new_grid)
+    return new_grid, changed
+
+
+def move_up(grid):
+
+    new_grid = transpose(grid)
+    new_grid, changed = move_left(new_grid)
+    new_grid = transpose(new_grid)
+    return new_grid, changed
+
+
+def move_down(grid):
+
+    new_grid = transpose(grid)
+    new_grid, changed = move_right(new_grid)
+    new_grid = transpose(new_grid)
+    return new_grid, changed
