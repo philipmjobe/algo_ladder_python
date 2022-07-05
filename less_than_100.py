9e9830847c98ec2a834146edc41bc01558ebe992
Philip <philipmjobe@gmail.com>
2022-07-05 16:33:07 -0400

    adds first_gui.py

--- less_than_100.py
+++ less_than_100.py
@@ -1,4 +1,15 @@
 # Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
 
-# Input: [99, 101, 88, 4, 2000, 50]
+input = [99, 101, 88, 4, 2000, 50]
 # Output: [99, 88, 4, 50]
+
+
+def less_than():
+    new_arr = []
+    for i in input:
+        if i < 100:
+            new_arr.append(i)
+            return new_arr
+
+
+print(less_than())
