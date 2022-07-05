06c53f00e8b616b37848bb035016cea6e8fe2544
Philip <philipmjobe@gmail.com>
2022-07-05 14:36:31 -0400

    adds pokemon.py and problem

---
+++ pokemon.py
@@ -0,0 +1,9 @@
+# Problem :
+# You are a Pokémon trainer. Each Pokémon has its own power, described by a positive integer value. As you travel, you watch Pokémon and you catch each of them. After each catch, you have to display maximum and minimum powers of Pokémon caught so far. You must have linear time complexity. So sorting won’t help here. Try having minimum extra space complexity.
+
+# Examples:
+# Suppose you catch Pokémon of powers 3 8 9 7. Then the output should be
+# 3 3
+# 3 8
+# 3 9
+# 3 9
