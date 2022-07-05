166a9f11b6d6116057590ef24b395eaee671ca73
Philip <philipmjobe@gmail.com>
2022-07-05 14:43:34 -0400

    adds implementation from geeksforgeeks

--- pokemon.py
+++ pokemon.py
@@ -7,3 +7,17 @@
 # 3 8
 # 3 9
 # 3 9
+
+powers = [3, 8, 9, 7]
+
+mini, maxi = 0, 0
+
+for power in powers:
+    if mini == 0 and maxi == 0:
+        mini, maxi == powers[0], powers[0]
+        print(mini, maxi)
+
+    else:
+        mini = min(mini, power)
+        maxi = max(maxi, power)
+        print(mini, maxi)
