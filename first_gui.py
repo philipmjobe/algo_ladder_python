d5305c1680a7ac56abccf389dda1e07427c05f56
Philip <philipmjobe@gmail.com>
2022-07-05 16:50:07 -0400

    adds a button that works on click and changes the message

--- first_gui.py
+++ first_gui.py
@@ -9,4 +9,13 @@ root.geometry('350x200')
 lbl = Label(root, text="Howdy")
 lbl.grid()
 
+
+def clicked():
+    lbl.configure(text="I've been clicked")
+
+
+btn = Button(root, text="Click me", fg="red", command=clicked)
+
+btn.grid(column=1, row=0)
+
 root.mainloop()
