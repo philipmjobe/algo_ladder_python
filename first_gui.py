a2afe8e79618eedf7ed021ec1f356dbfac14a029
Philip <philipmjobe@gmail.com>
2022-07-05 16:55:23 -0400

    adds space for person to type in a word and have it added to the end of a sentence

--- first_gui.py
+++ first_gui.py
@@ -9,13 +9,17 @@ root.geometry('350x200')
 lbl = Label(root, text="Howdy")
 lbl.grid()
 
+txt = Entry(root, width=10)
+txt.grid(column=1, row=0)
+
 
 def clicked():
-    lbl.configure(text="I've been clicked")
+    res = "You wrote" + txt.get()
+    lbl.configure(text=res)
 
 
 btn = Button(root, text="Click me", fg="red", command=clicked)
 
-btn.grid(column=1, row=0)
+btn.grid(column=2, row=0)
 
 root.mainloop()
