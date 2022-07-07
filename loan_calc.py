8d317ecafe4b12f0182c9968781b13ad4466bfa2
Philip <philipmjobe@gmail.com>
2022-07-07 15:07:06 -0400

    adds loan_calc.py and implementation steps

---
+++ loan_calc.py
@@ -0,0 +1,9 @@
+# Python offers multiple options for developing GUI (Graphical User Interface). Out of all the GUI methods, Tkinter is the most commonly used method. It is a standard Python interface to the Tk GUI toolkit shipped with Python. Python with Tkinter outputs the fastest and easiest way to create GUI applications. Creating a GUI using Tkinter is an easy task.
+
+
+# To create a Tkinter :
+
+# Importing the module – Tkinter
+# Create the main window (container)
+# Add any number of widgets to the main window
+# Apply the event Trigger on the widgets.
