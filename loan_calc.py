7719e2eaf39631614bb74c05135b5c667e708167
Philip <philipmjobe@gmail.com>
2022-07-07 16:02:45 -0400

    sets up the window

--- loan_calc.py
+++ loan_calc.py
@@ -7,3 +7,48 @@
 # Create the main window (container)
 # Add any number of widgets to the main window
 # Apply the event Trigger on the widgets.
+
+
+from tkinter import StringVar
+from tkinter.ttk import Label
+
+
+def __init__(self):
+
+    window = Tk()
+    window.title("Loan Calculator")
+
+    Label(window, text="Annual Interest Rate").grid(row=1, column=1, sticky=W)
+
+    Label(window, text="Number of Years").grid(row=2, column=1, sticky=W)
+
+    Label(window, text="Loan Amount").grid(row=3, column=1, sticky=W)
+
+    Label(window, text="Monthly Payment").grid(row=4, column=1, sticky=W)
+
+    Label(window, text="Total Payment").grid(row=5, column=1, sticky=W)
+
+    self.annualInterestRateVar = StringVar()
+    Entry(window, textvariable=self.annualInterestRateVar,
+          justify=RIGHT).grid(row=1, column=2)
+
+    self.numberOfYearsVar = StringVar()
+    Entry(window, textvariable=self.numberOfYearsVar,
+          justify=RIGHT).grid(row=2, column=2)
+
+    self.loanAmountVar = StringVar()
+    Entry(window, textvariable=self.loanAmountVar,
+          justify=RIGHT).grid(row=3, column=2)
+
+    self.monthlyPaymentVar = StringVar()
+    lblMonthlyPayment = Label(window, textvariable=self.monthlyPaymentVar,
+                              ).grid(row=4, column=2, sticky=E)
+
+    self.totalPaymentVar = StringVar()
+    lblTotalPayment = Label(window, textvariable=self.totalPaymentVar).grid(
+        row=5, column=2, sticky=E)
+
+    btComputePayment = Button(window, text="Compute Payment",
+                              command=self.computePayment).grid(row=6, column=2, sticky=E)
+
+    window.mainloop()
