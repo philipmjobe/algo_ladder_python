d86f76312cda90adb5d8a13841f630c6a21478a3
Philip <philipmjobe@gmail.com>
2022-07-08 13:59:02 -0400

    adds rank_calc.py and implementation steps

---
+++ rank_calc.py
@@ -0,0 +1,7 @@
+# Python offers multiple options for developing a GUI (Graphical User Interface). Out of all the GUI methods, Tkinter is the most commonly used method. In this article, we will learn how to create a Rank Based – Percentile Gui Calculator application using Tkinter, with a step-by-step guide.
+# To create a Tkinter:
+
+# Importing the module – Tkinter
+# Create the main window (container)
+# Add any number of widgets to the main window.
+# Apply the event Trigger on the widgets.
