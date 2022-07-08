9869665aae2aa7d360ec350f24a75da84ccb55a9
Philip <philipmjobe@gmail.com>
2022-07-08 13:56:39 -0400

    adds final functionality to loan_calc.py

--- loan_calc.py
+++ loan_calc.py
@@ -9,65 +9,70 @@
 # Apply the event Trigger on the widgets.
 
 
-from tkinter import StringVar
-from tkinter.ttk import Label
+from tkinter import *
 
 
-def __init__(self):
+class LoanCalculator:
 
-    window = Tk()
-    window.title("Loan Calculator")
+    def __init__(self):
 
-    Label(window, text="Annual Interest Rate").grid(row=1, column=1, sticky=W)
+        window = Tk()
+        window.title("Loan Calculator")
 
-    Label(window, text="Number of Years").grid(row=2, column=1, sticky=W)
+        Label(window, text="Annual Interest Rate").grid(
+            row=1, column=1, sticky=W)
 
-    Label(window, text="Loan Amount").grid(row=3, column=1, sticky=W)
+        Label(window, text="Number of Years").grid(row=2, column=1, sticky=W)
 
-    Label(window, text="Monthly Payment").grid(row=4, column=1, sticky=W)
+        Label(window, text="Loan Amount").grid(row=3, column=1, sticky=W)
 
-    Label(window, text="Total Payment").grid(row=5, column=1, sticky=W)
+        Label(window, text="Monthly Payment").grid(row=4, column=1, sticky=W)
 
-    self.annualInterestRateVar = StringVar()
-    Entry(window, textvariable=self.annualInterestRateVar,
-          justify=RIGHT).grid(row=1, column=2)
+        Label(window, text="Total Payment").grid(row=5, column=1, sticky=W)
 
-    self.numberOfYearsVar = StringVar()
-    Entry(window, textvariable=self.numberOfYearsVar,
-          justify=RIGHT).grid(row=2, column=2)
+        self.annualInterestRateVar = StringVar()
+        Entry(window, textvariable=self.annualInterestRateVar,
+              justify=RIGHT).grid(row=1, column=2)
 
-    self.loanAmountVar = StringVar()
-    Entry(window, textvariable=self.loanAmountVar,
-          justify=RIGHT).grid(row=3, column=2)
+        self.numberOfYearsVar = StringVar()
+        Entry(window, textvariable=self.numberOfYearsVar,
+              justify=RIGHT).grid(row=2, column=2)
 
-    self.monthlyPaymentVar = StringVar()
-    lblMonthlyPayment = Label(window, textvariable=self.monthlyPaymentVar,
-                              ).grid(row=4, column=2, sticky=E)
+        self.loanAmountVar = StringVar()
+        Entry(window, textvariable=self.loanAmountVar,
+              justify=RIGHT).grid(row=3, column=2)
 
-    self.totalPaymentVar = StringVar()
-    lblTotalPayment = Label(window, textvariable=self.totalPaymentVar).grid(
-        row=5, column=2, sticky=E)
+        self.monthlyPaymentVar = StringVar()
+        lblMonthlyPayment = Label(window, textvariable=self.monthlyPaymentVar,
+                                  ).grid(row=4, column=2, sticky=E)
 
-    btComputePayment = Button(window, text="Compute Payment",
-                              command=self.computePayment).grid(row=6, column=2, sticky=E)
+        self.totalPaymentVar = StringVar()
+        lblTotalPayment = Label(window, textvariable=self.totalPaymentVar).grid(
+            row=5, column=2, sticky=E)
 
-    window.mainloop()
+        btComputePayment = Button(window, text="Compute Payment",
+                                  command=self.computePayment).grid(row=6, column=2, sticky=E)
+
+        window.mainloop()
+
+    def computePayment(self):
+
+        monthlyPayment = self.getMonthlyPayment(float(self.loanAmountVar.get()), float(
+            self.annualInterestRateVar.get()) / 1200, int(self.numberOfYearsVar.get))
+
+        self.monthlyPaymentVar.set(format(monthlyPayment, '10.2f'))
+        totalPayment = float(self.monthlyPaymentVar.get()) * \
+            12 * int(self.numberOfYearsVar.get())
+
+        self.totalPaymentVar.set(format(totalPayment, '10.2f'))
+
+        def getMonthlyPayment(self, loanAmount, monthlyInterestRate, numberOfYears):
+            monthlyPayment = loanAmount * monthlyInterestRate / \
+                (1 - 1 / (1 + monthlyInterestRate) ** (numberOfYears))
+
+        return monthlyPayment
+
+        root = Tk()
 
 
-def computePayment(self):
-
-    monthlyPayment = self.getMonthlyPayment(float(self.loanAmountVar.get()), float(
-        self.annualInterestRateVar.get()) / 1200, int(self.numberOfYearsVar.get))
-
-    self.monthlyPaymentVar.set(format(monthlyPayment, '10.2f'))
-    totalPayment = float(self.monthlyPaymentVar.get()) * \
-        12 * int(self.numberOfYearsVar.get())
-
-    self.totalPaymentVar.set(format(totalPayment, '10.2f'))
-
-
-def getMonthlyPayment(self, loanAmount, monthlyInterestRate, numberOfYears):
-    monthlyPayment = loanAmount * monthlyInterestRate / \
-        (1 - 1 / (1 + monthlyInterestRate) ** (numberOfYears))
-
-    return monthlyPayment
+LoanCalculator()
