5f99d1678e2ed05afa4378e56aaaa6cabcab0582
Philip <philipmjobe@gmail.com>
2022-07-08 14:39:53 -0400

    adds standard_converter.py and implementation steps

---
+++ standard_converter.py
@@ -0,0 +1,10 @@
+# In this article, we will learn how to create a standard converter using tkinter. Now we are going to create an introduction window that displays loading bar, welcome text, and user’s social media profile links so that when he/she shares his code with some others, they can contact the author using those resources. It looks like a bit lengthy code, but believe me, guys if you start understanding it is so easy and i divided the code into blocks which helps you to understand better.
+
+# Steps to create an introduction window:
+# First of all, “Tkinter” and “webbrowser” modules to be imported.
+# Create an intro class that fires the introduction window.
+# Create a Toplevel Tkinter window in order to use full features of a window.
+# Place a welcome label on the top of the window.
+# Create one “ttk.Progressbar” which gives us loading effect.
+# Finally, create four buttons and provide your social media links using “webbrowser” module.
+# And you have to download/create four images to represent your social media links.
