45fa090b8db5fb212cad0c73adbfcd9129f8df9a
Philip <philipmjobe@gmail.com>
2022-07-13 14:33:27 -0400

    adds logic to create table from geekforgeeks to tkinter_table.py

--- tkinter_table.py
+++ tkinter_table.py
@@ -1,2 +1,30 @@
 # Creating Tables Using Tkinter
 # A table is useful to display data in the form of rows and columns. Unfortunately, Tkinter does not provide a Table widget to create a table. But we can create a table using alternate methods. For example, we can make a table by repeatedly displaying entry widgets in the form of rows and columns.
+
+from tkinter import *
+
+
+class Table:
+
+    def __init__(self, root):
+        for i in range(total_rows):
+            for j in range(total_columns):
+                self.e = Entry(root, width=20, fg="blue",
+                               font=('Arial', 16, 'bold'))
+
+                self.e.grid(row=i, column=j)
+                self.e.insert(END, lst[i][j])
+
+
+lst = [(1, 'Raj', 'Mumbai', 19),
+       (2, 'Aaryan', 'Pune', 18),
+       (3, 'Vaishnavi', 'Mumbai', 20),
+       (4, 'Rachna', 'Mumbai', 21),
+       (5, 'Shubham', 'Delhi', 21)]
+
+total_rows = len(lst)
+total_columns = len(lst[0])
+
+root = Tk()
+t = Table(root)
+root.mainloop()
