bb57ef410d7c4b8772afc166a189e36b6f67836e
Philip <philipmjobe@gmail.com>
2022-07-13 14:13:42 -0400

    adds tkinter_table.py and implementation steps from geeksforgeeks

---
+++ tkinter_table.py
@@ -0,0 +1,2 @@
+# Creating Tables Using Tkinter
+# A table is useful to display data in the form of rows and columns. Unfortunately, Tkinter does not provide a Table widget to create a table. But we can create a table using alternate methods. For example, we can make a table by repeatedly displaying entry widgets in the form of rows and columns.
