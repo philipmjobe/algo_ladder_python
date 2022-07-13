d07296a829c37e97080a075baf2a75494f7c35c3
Philip <philipmjobe@gmail.com>
2022-07-13 16:13:37 -0400

    adds todo.py and implementation steps

---
+++ todo.py
@@ -0,0 +1,8 @@
+# Python offers multiple options for developing GUI (Graphical User Interface). Out of all the GUI methods, Tkinter is the most commonly used method. In this article, we will learn how to create a ToDo GUI application using Tkinter, with a step-by-step guide.
+
+# To create a tkinter :
+
+# Importing the module – tkinter
+# Create the main window (container)
+# Add any number of widgets to the main window.
+# Apply the event Trigger on the widgets.
