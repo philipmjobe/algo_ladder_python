69b597f971a64a33817c6bb35905cbbb4c123a0e
Philip <philipmjobe@gmail.com>
2022-07-13 14:47:39 -0400

    adds gui_calendar.py and implementation steps

---
+++ gui_calendar.py
@@ -0,0 +1,9 @@
+# Python offers multiple options for developing a GUI (Graphical User Interface). Out of all the GUI methods, Tkinter is the most commonly used method. Python with Tkinter outputs the fastest and easiest way to create GUI applications. In this article, we will learn how to create a GUI Calendar application using Tkinter, with a step-by-step guide.
+
+
+# To create a Tkinter :
+
+# Importing the module – Tkinter
+# Create the main window (container)
+# Add any number of widgets to the main window.
+# Apply the event Trigger on the widgets.
