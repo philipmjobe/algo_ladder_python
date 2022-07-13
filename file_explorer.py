8ad5bc63d8ac8376c3290aaab68de2783d743c5f
Philip <philipmjobe@gmail.com>
2022-07-13 15:09:55 -0400

    adds file_explorer.py and implementation steps

---
+++ file_explorer.py
@@ -0,0 +1,9 @@
+# Python offers various modules to create graphics programs. Out of these Tkinter provides the fastest and easiest way to create GUI applications.
+
+
+# The following steps are involved in creating a tkinter application:
+
+# Importing the Tkinter module.
+# Creation of the main window (container).
+# Addition of widgets to the main window
+# Applying the event Trigger on widgets like buttons, etc.
