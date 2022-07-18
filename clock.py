61db3e6af31959b621e8d77c648c39e13f409143
Philip <philipmjobe@gmail.com>
2022-07-18 15:31:07 -0400

    adds clock.py and adds implementation steps

---
+++ clock.py
@@ -0,0 +1,2 @@
+# Using Label widget from Tkinter and time module :
+# In the following application, we are going to use Label widget and also going to use time module which we will use to retrieve system’s time.
