175b3e3d7d7c407ea8e540f70863091f2d8bc7a1
Philip <philipmjobe@gmail.com>
2022-07-18 15:15:28 -0400

    adds weight_converter.py and implementation steps

--- less_than_100.py
+++ less_than_100.py
@@ -4,12 +4,11 @@ input = [99, 101, 88, 4, 2000, 50]
 # Output: [99, 88, 4, 50]
 
 
-def less_than():
+def less_than(input):
     new_arr = []
-    for i in input:
+    for i, input in enumerate(input):
         if i < 100:
             new_arr.append(i)
-            return new_arr
 
 
-print(less_than())
+print(less_than(input))
