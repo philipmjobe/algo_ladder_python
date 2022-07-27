e802d4f3af36150cdea5d3605ea0841373923c95
Philip <philipmjobe@gmail.com>
2022-07-27 14:07:29 -0400

    adds allLinks to scraper to pull other links from the wiki

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import random
 
 response = requests.get(
     url="https://en.wikipedia.org/wiki/Web_scraping",
@@ -7,4 +8,16 @@ response = requests.get(
 soup = BeautifulSoup(response.content, 'html.parser')
 
 title = soup.find(id="firstHeading")
-print(title.string)
+print(title.content)
+
+allLinks = soup.find(id="bodyContent").find_all("a")
+random.shuffle(allLinks)
+linkToScrape = 0
+
+for link in allLinks:
+    if link['href'].find("/wiki") == 1:
+        continue
+
+    linkToScrape = link
+    break
+print(linkToScrape)
