4061402cbf70f3398ca23dce595ef4eabf5e44b9
Philip <philipmjobe@gmail.com>
2022-07-27 14:17:48 -0400

    adds new scraping method to pull many article titles

--- scraper.py
+++ scraper.py
@@ -2,22 +2,48 @@ import requests
 from bs4 import BeautifulSoup
 import random
 
-response = requests.get(
-    url="https://en.wikipedia.org/wiki/Web_scraping",
-)
-soup = BeautifulSoup(response.content, 'html.parser')
+# response = requests.get(
+#     url="https://en.wikipedia.org/wiki/Web_scraping",
+# )
+# soup = BeautifulSoup(response.content, 'html.parser')
 
-title = soup.find(id="firstHeading")
-print(title.content)
+# title = soup.find(id="firstHeading")
+# print(title.content)
 
-allLinks = soup.find(id="bodyContent").find_all("a")
-random.shuffle(allLinks)
-linkToScrape = 0
+# allLinks = soup.find(id="bodyContent").find_all("a")
+# random.shuffle(allLinks)
+# linkToScrape = 0
 
-for link in allLinks:
-    if link['href'].find("/wiki") == 1:
-        continue
+# for link in allLinks:
+#     if link['href'].find("/wiki") == 1:
+#         continue
 
-    linkToScrape = link
-    break
-print(linkToScrape)
+#     linkToScrape = link
+#     break
+# print(linkToScrape)
+
+
+def scrapeWikiArticle(url):
+    response = requests.get(
+        url=url,
+    )
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    title = soup.find(id="firstHeading")
+    print(title.text)
+
+    allLinks = soup.find(id="bodyContent").find_all("a")
+    random.shuffle(allLinks)
+    linkToScrape = 0
+
+    for link in allLinks:
+        if link['href'].find("/wiki") == -1:
+            continue
+
+        linkToScrape = link
+        break
+
+    scrapeWikiArticle("https://en.wikipedia.org" + linkToScrape['href'])
+
+
+scrapeWikiArticle("https://en.wikipedia.org/wiki/Web_scraper")
