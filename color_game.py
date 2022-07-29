8b0cfed43416140e15157ca729af8b4237ffbf71
Philip <philipmjobe@gmail.com>
2022-07-29 12:45:40 -0400

    adds function to choose and display the next color

--- color_game.py
+++ color_game.py
@@ -1,5 +1,6 @@
 # Let’s try to make a game using Tkinter. In this game player has to enter color of the word that appears on the screen and hence the score increases by one, the total time to play this game is 30 seconds. Colors used in this game are Red, Blue, Green, Pink, Black, Yellow, Orange, White, Purple and Brown. Interface will display name of different colors in different colors. Player has to identify the color and enter the correct color name to win the game.
 
+
 import tkinter
 import random
 
@@ -16,3 +17,21 @@ def startGame(event):
         countdown()
 
     nextColor()
+
+
+def nextColor():
+    global score
+    global timeleft
+
+    if timeleft > 0:
+        e.focus_set()
+        if e.get().lower() == colors[1].lower():
+            score += 1
+
+        e.delete(0, tkinter.END)
+
+        random.shuffle(colors)
+
+        label.config(fg=str(colors[1]), text=str(colors[0]))
+
+        scoreLabel.config(text="Score: " + str(score))
