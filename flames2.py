cb586d9b94c5e6667b8126a1cfb597b391ebf343
Philip <philipmjobe@gmail.com>
2022-07-29 13:22:02 -0400

    adds function for telling the relationship status

--- flames2.py
+++ flames2.py
@@ -17,4 +17,26 @@ def remove_match_char(list1, list2):
                 return [list3, True]
 
     list3 = list1 + ["*"] + list2
-    return [lsit3, False]
+    return [list3, False]
+
+
+def tell_status():
+    p1 = Player1_field.get()
+    p1 = p1.lower()
+    p1.replace(" ", "")
+    p1_list = list(p1)
+
+    p2 = Player2_field.get()
+    p2 = p2.lower()
+    p2.replace(" ", "")
+    p2_list = list(p2)
+
+    proceed = True
+
+    while proceed:
+        ret_list = remove_match_char(p1_list, p2_list)
+        con_list = ret_list[0]
+        proceed = ret_list[1]
+        star_index = con_list.index("*")
+        p1_list = con_list[: star_index]
+        p2_list = con_list[star_index + 1:]
