230190cc612432adc72f615c90628c3c627b9d3f
Philip <philipmjobe@gmail.com>
2022-07-29 12:48:05 -0400

    adds countdown function

--- color_game.py
+++ color_game.py
@@ -35,3 +35,14 @@ def nextColor():
         label.config(fg=str(colors[1]), text=str(colors[0]))
 
         scoreLabel.config(text="Score: " + str(score))
+
+
+def countdown():
+    global timeleft
+
+    if timeleft > 0:
+        timeleft -= 1
+
+        timeLabel.config(text="Time left: " + str(timeleft))
+
+        timeLabel.after(1000, countdown)
