88dd800b6c4f0413d0d44065d3d4e3396af5c091
Philip <philipmjobe@gmail.com>
2022-08-01 13:35:01 -0400

    adds covid_tracker.py and how it works

---
+++ covid_tracker.py
@@ -0,0 +1,7 @@
+# Sometimes we just want a quick fast tool to really tell whats the current update, we just need a bare minimum of data. Web scrapping deals with taking some data from the web and then processing it and displaying the relevant content in a short and crisp manner.
+# What the code is doing ?
+
+
+# First we are using Tkinter Library to make GUI required for our script
+# We are using requests Library to get the data from the unofficial api
+# Then we are displaying the data we need in this case its Total active cases: and confirmed cases
