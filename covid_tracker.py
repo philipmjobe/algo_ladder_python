61ef52df7024fce5f8b3514e3efc4d0d67402743
Philip <philipmjobe@gmail.com>
2022-08-01 13:45:00 -0400

    adds implementation from tutorial and it does not work but I think it is just the api that it is pulling from

--- covid_tracker.py
+++ covid_tracker.py
@@ -5,3 +5,38 @@
 # First we are using Tkinter Library to make GUI required for our script
 # We are using requests Library to get the data from the unofficial api
 # Then we are displaying the data we need in this case its Total active cases: and confirmed cases
+
+import requests
+import json
+from tkinter import *
+
+window = Tk()
+
+window.title("Covid-19")
+window.geometry('220x70')
+
+lbl = Label(window, text="Total active cases:-......")
+lbl1 = Label(window, text="Total confirmed cases:-...")
+
+lbl.grid(column=1, row=0)
+lbl1.grid(column=1, row=1)
+lbl2 = Label(window, text="")
+lbl2.grid(column=1, row=3)
+
+
+def clicked():
+
+    url = "https://api.covid19india.org / data.json"
+    page = requests.get(url)
+    data = json.loads(page.text)
+
+    lbl.configure(text="Total active cases:-" + data["statewise"][0]["active"])
+    lbl1.configure(text="Total confirmed cases:-" +
+                   data["statewise"][0]["confirmed"])
+    lbl2.configure(text="Data refreshed")
+
+
+btn = Button(window, text="Refresh", command=clicked)
+btn.grid(column=2, row=0)
+
+window.mainloop()
