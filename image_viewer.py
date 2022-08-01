33ab64cd6f75cfc6694bc978451507787f8e8de9
Philip <philipmjobe@gmail.com>
2022-08-01 13:08:06 -0400

    adds forward function adding functionality to the forward button

--- image_viewer.py
+++ image_viewer.py
@@ -3,6 +3,29 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
+
+def forward(img_no):
+    global label
+    global button_forward
+    global button_back
+    global button_exit
+    label.grid_forget()
+
+    label = Label(image=List_images[img_no-1])
+
+    label.grid(row=1, column=0, columnspan=3)
+    button_for = Button(root, text="forward",
+                        command=lambda: forward(img_no+1))
+
+    if img_no == 4:
+        button_forward = Button(root, text="Forward", state=DISABLED)
+
+    button_back = Button(root, text="Back", command=lambda: back(img_no-1))
+    button_back.grid(row=5, column=0)
+    button_exit.grid(row=5, column=1)
+    button_for.grid(row=5, column=2)
+
+
 root = Tk()
 
 root.title("Image Viewer")
