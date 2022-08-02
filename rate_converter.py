812c350e59ef88a57e7fe58ed61c99314bbc38dd
Philip <philipmjobe@gmail.com>
2022-08-02 12:33:07 -0400

    adds rate_converter.py and implementation steps

---
+++ rate_converter.py
@@ -0,0 +1,4 @@
+# Extract data from the given URL. Copy the URL, after selecting the desired location.
+# Scrape the data with the help of requests and the Beautiful Soup module.
+# Convert that data into HTML code.
+# Find the required details and filter them.
