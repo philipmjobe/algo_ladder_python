8d237a35599e8b7a5fa19c973dfba05c79330a48
Philip <philipmjobe@gmail.com>
2022-08-02 13:14:04 -0400

    adds implementation and trys to test but I think it was built for windows not mac

--- rotate_screen.py
+++ rotate_screen.py
@@ -1,1 +1,35 @@
 # The display can be modified to four orientations using some methods from the rotatescreen module, it is a small Python package for rotating the screen in a system.
+
+from tkinter import *
+import rotatescreen
+
+
+def Screen_rotation(temp):
+    screen = rotatescreen.get_primary_display()
+    if temp == "up":
+        screen.set_landscape()
+    elif temp == "right":
+        screen.set_portrait_flipped()
+    elif temp == "down":
+        screen.set_landscape_flipped()
+    elif temp == "left":
+        screen.set_portrait()
+
+
+master = Tk()
+master.geometry("100x100")
+master.title("Screen Rotation")
+master.configure(bg="light grey")
+
+result = StringVar()
+
+Button(master, text="Up", command=lambda: Screen_rotation(
+    "up"), bg="white").grid(row=0, column=3)
+Button(master, text="Right", command=lambda: Screen_rotation(
+    "right"), bg="white").grid(row=1, column=6)
+Button(master, text="Left", command=lambda: Screen_rotation(
+    "left"), bg="white").grid(row=1, column=2)
+Button(master, text="Down", command=lambda: Screen_rotation(
+    "down"), bg="white").grid(row=3, column=3)
+
+mainloop()
