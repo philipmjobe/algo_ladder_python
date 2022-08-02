223b56464573c45c7494f5fd3de353541030c2aa
Philip <philipmjobe@gmail.com>
2022-08-02 12:42:38 -0400

    imports all modules, creates url get function, passes url into get function, filters currency details

--- rate_converter.py
+++ rate_converter.py
@@ -2,3 +2,29 @@
 # Scrape the data with the help of requests and the Beautiful Soup module.
 # Convert that data into HTML code.
 # Find the required details and filter them.
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def getdata(url):
+    r = requests.get(url)
+    return r.text
+
+
+def get_info():
+    try:
+        htmldata = getdata("https://finance.yahoo.com/quote/usdinr=X?ltr=1")
+        soup = BeautifulSoup(htmldata, 'html.parser')
+        mydatastr = ''
+        for table in soup.find_all("div", class_="D(ib) Va(m) Maw(65%) Ov(h)"):
+            mydatastr += table.get_text()
+        list_data = mydatastr.split()
+        temp = list_data[0].split("-")
+        rate.set(temp[0])
+        inc.set(temp[1])
+        per_rate.set(list_data[1])
+        time.set(list_data[3])
+        result.set("sucess")
+    except:
+        result.set("Oops! something wrong")
