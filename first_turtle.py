86f4406f764ea08d21f4431d7399e5bd7110175c
Philip <philipmjobe@gmail.com>
2022-08-05 14:35:45 -0400

    adds first_turtle.py and explanation

---
+++ first_turtle.py
@@ -0,0 +1,1 @@
+# turtle is an inbuilt module in Python. It provides drawing using a screen (cardboard) and turtle (pen). To draw something on the screen, we need to move the turtle (pen). To move turtle, there are some functions i.e forward(), backward(), etc.
--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,5 @@
 print("Hello World")
+
+thislist = ["a", "b", "c"]
+thislist.clear()
+print(thislist)
