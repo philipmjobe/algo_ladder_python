baf20dfc70301ecad98bcaa7577d9352b39bbe96
Philip <philipmjobe@gmail.com>
2022-08-05 15:25:51 -0400

    adds for loop to build a polygon of any size

--- polygon_turtle.py
+++ polygon_turtle.py
@@ -1,1 +1,11 @@
 # Turtle is an inbuilt module of python. It enables us to draw any drawing by a turtle and methods defined in the turtle module and by using some logical loops. turtle drawings are basically drawn using four methods defined in the turtle module.
+import turtle
+
+t = turtle.Turtle()
+
+n = int(input("Enter the no if the sides of the polygon : "))
+l = int(input("Enter the length of the sides of the polygon : "))
+
+for _ in range(n):
+    turtle.forward(1)
+    turtle.right(360 / n)
