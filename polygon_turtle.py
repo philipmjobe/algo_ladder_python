c61dfad47f61a72d0d60a689ffc1f5dcbfa4527a
Philip <philipmjobe@gmail.com>
2022-08-05 15:22:07 -0400

    adds polygon_turtle.py and explanation

---
+++ polygon_turtle.py
@@ -0,0 +1,1 @@
+# Turtle is an inbuilt module of python. It enables us to draw any drawing by a turtle and methods defined in the turtle module and by using some logical loops. turtle drawings are basically drawn using four methods defined in the turtle module.
