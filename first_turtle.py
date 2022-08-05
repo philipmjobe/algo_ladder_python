600da097ef3bb00e15749bd90dcb3aed2881b186
Philip <philipmjobe@gmail.com>
2022-08-05 14:40:00 -0400

    adds implementation of drawing a square and it works really smoothly

--- first_turtle.py
+++ first_turtle.py
@@ -1,1 +1,21 @@
 # turtle is an inbuilt module in Python. It provides drawing using a screen (cardboard) and turtle (pen). To draw something on the screen, we need to move the turtle (pen). To move turtle, there are some functions i.e forward(), backward(), etc.
+
+# DRAWING A SQUARE
+
+import turtle
+
+t = turtle.Turtle()
+
+s = int(input("Enter the length of the side of the Square: "))
+
+t.forward(s)
+t.left(90)
+
+t.forward(s)
+t.left(90)
+
+t.forward(s)
+t.left(90)
+
+t.forward(s)
+t.left(90)
