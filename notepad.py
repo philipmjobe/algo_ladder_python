b588f431f814e9ec13f2c3dfe4031451b1ee3d80
Philip <philipmjobe@gmail.com>
2022-12-05 14:32:43 -0500

    adds notepad.py and implementation steps

---
+++ notepad.py
@@ -0,0 +1,3 @@
+# Let’s see how to create a simple notepad in Python using Tkinter. This notepad GUI will consist of various menu like file and edit, using which all functionalities like saving the file, opening a file, editing, cut and paste can be done.
+
+# Now for creating this notepad, Python 3 and Tkinter should already be installed in your system. You can download suitable python package as per system requirement. After you have successfully installed python you need to install Tkinter (a Python’s GUI package)
