e0ccf4775967ae89b7c445a66fc25ef6a9708542
Philip <philipmjobe@gmail.com>
2022-12-05 15:09:52 -0500

    starts adding logic for note pad

--- notepad.py
+++ notepad.py
@@ -7,3 +7,63 @@ import os
 from tkinter import *
 from tkinter.messagebox import *
 from tkinter.filedialog import *
+
+
+class Notepad:
+    __root = Tk()
+
+    __thisWidth = 300
+    __thisHeight = 300
+    __thisTextArea = Text(__root)
+    __thisMenuBar = Menu(__root)
+    __thisFileMenu = Menu(__thisMenuBar, tearoff=0)
+    __thisEditMenu = Menu(__thisMenuBar, tearoff=0)
+    __thisHelpMenu = Menu(__thisMenuBar, tearoff=0)
+
+    __thisScrollBar = Scrollbar(__thisTextArea)
+    __file = None
+
+    def __init__(self, **kwargs):
+        try:
+            self.__root.wm_iconbitmap("Notepad.ico")
+        except:
+            pass
+
+        try:
+            self.__thisWidth = kwargs['width']
+        except KeyError:
+            pass
+
+        try:
+            self.__thisHeight = kwargs['height']
+        except KeyError:
+            pass
+
+        self.__root.title("Untitled - Notepad")
+
+        screenWidth = self.__root.winfo_screenmmwidth()
+        screenHeight = self.__root.winfo_screenheight()
+
+        left = (screenWidth / 2) - (self.__thisWidth / 2)
+
+        top = (screenHeight / 2) - (self.__thisHeight / 2)
+
+        self.__root.geometry('%dx%d+%d+%d' %
+                             (self.__thisWidth, self.__thisHeight, left, top))
+
+        self.__root.grid_rowconfigure(0, weight=1)
+        self.__root.grid_columnconfigure(0, weight=1)
+
+        self.__thisTextArea.grid(sticky=N + E + S + W)
+
+        self.__thisFileMenu.add_command(label="New", command=self.__newFile)
+        self.__thisFileMenu.add_command(labe="Open", command=self.__openFile)
+        self.__thisFileMenu.add_command(label="Save", command=self.__saveFile)
+        self.__thisFileMenu.add_separator()
+        self.__thisFileMenu.add_command(
+            label="Exit", command=self.__quitApplication)
+        self.__thisMenuBar.add_cascade(label="File", menu=self.__thisFileMenu)
+        self.__thisEditMenu.add_command(label="Cut", command=self.__cut)
+        self.__thisEditMenu.add_command(label="Copy", command=self.__copy)
+        self.__thisEditMenu.add_command(label="Paste", command=self.__paste)
+        self.
